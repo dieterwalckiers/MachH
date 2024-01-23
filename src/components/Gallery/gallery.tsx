d0e6c9fe4c6c8840cc4f4843771d55b78482c68c
Dieter Walckiers <dieter@manual.to>
2024-01-23 20:05:58 +0100

    layout fix in arrow navigation in Gallery

--- src/components/Gallery/gallery.tsx
+++ src/components/Gallery/gallery.tsx
@@ -61,33 +61,26 @@ const Gallery = component$<Props>(({ images }) => {
                             detailVisible.value = false;
                         }}
                     >
-                        <div class="w-full md:w-[600px]">
+                        <div class="w-full md:w-[600px] flex items-center relative">
+                            <div class={`absolute left-[1rem] bottom-[-3rem] md:left-[-4rem] md:bottom-[initial] left-button cursor-pointer ${activeImgPos.value <= 0 ? "invisible" : ""}`} onClick$={(e) => { activeImgPos.value = activeImgPos.value - 1; e.stopPropagation(); }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M10.707 3.293a1 1 0 010 1.414L6.414 9H17a1 1 0 110 2H6.414l4.293 4.293a1 1 0 01-1.414 1.414l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 0z" clip-rule="evenodd" />
+                                </svg>
+                            </div>
                             <MachHImage
                                 image={activeImg.value}
                                 key={`galimg${activeImgPos.value}`}
                                 alt={`images image 0`}
                                 // eslint-disable-next-line qwik/no-react-props
-                                className="float-left mr-4 mb-4"
                                 // width={isMobile(mainCtx.screenSize) ? 400 : 1200}
                                 maxDim={isMobile(mainCtx.screenSize) ? 400 : 1200}
                                 objectFit="contain"
                                 resolutionsOverride={[400, 1200]}
                             />
-                            <div class="flex justify-between w-full">
-                                {activeImgPos.value > 0 ? (
-                                    <div class="left-button cursor-pointer" onClick$={(e) => { activeImgPos.value = activeImgPos.value - 1; e.stopPropagation(); }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M10.707 3.293a1 1 0 010 1.414L6.414 9H17a1 1 0 110 2H6.414l4.293 4.293a1 1 0 01-1.414 1.414l-6-6a1 1 0 010-1.414l6-6a1 1 0 011.414 0z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                ) : <div></div>}
-                                {activeImgPos.value < images.length - 1 ? (
-                                    <div class="right-button cursor-pointer" onClick$={(e) => { activeImgPos.value = activeImgPos.value + 1; e.stopPropagation(); }}>
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" viewBox="0 0 20 20" fill="currentColor">
-                                            <path fill-rule="evenodd" d="M9.293 16.707a1 1 0 010-1.414L13.586 11H3a1 1 0 010-2h10.586l-4.293-4.293a1 1 0 111.414-1.414l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-                                        </svg>
-                                    </div>
-                                ) : <div></div>}
+                            <div class={`absolute right-[1rem] bottom-[-3rem] md:right-[-4rem] md:bottom-[initial] right-button cursor-pointer ${activeImgPos.value >= images.length - 1 ? "invisible" : ""}`} onClick$={(e) => { activeImgPos.value = activeImgPos.value + 1; e.stopPropagation(); }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-12 w-12 text-white" viewBox="0 0 20 20" fill="currentColor">
+                                    <path fill-rule="evenodd" d="M9.293 16.707a1 1 0 010-1.414L13.586 11H3a1 1 0 010-2h10.586l-4.293-4.293a1 1 0 111.414-1.414l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                                </svg>
                             </div>
                         </div>
                     </div>
