a16cb63cd06ba7e15f253609d5a7df302d3ba973
Dieter Walckiers <dieter@manual.to>
2025-05-20 23:30:56 +0200

    Revert "attempt fix build"
    This reverts commit 93240e222a5ca65391a9f8933e5d8112a7e1c074.

--- vite.config.ts
+++ vite.config.ts
@@ -15,10 +15,5 @@ export default defineConfig(() => {
         "Cache-Control": "public, max-age=600",
       },
     },
-    ssr: {
-      noExternal: [
-        "nodemailer"
-      ]
-    }
   };
 });
