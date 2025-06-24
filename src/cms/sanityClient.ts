b224b8bff8b017beb0a74c696f2b3c2d39931c08
Dieter Walckiers <dieter@manual.to>
2025-06-24 13:01:58 +0200

    attempt to fix mollie webhook post

--- src/cms/sanityClient.ts
+++ src/cms/sanityClient.ts
@@ -1,9 +1,11 @@
-import { createClient } from '@sanity/client'
+/* eslint-disable @typescript-eslint/no-var-requires */
+// Note: CommonJS style is required for this file to work with Vercel edge function runtime
+const { createClient: createSanityClient } = require('@sanity/client');
 
-export default createClient({
+module.exports = createSanityClient({
     projectId: 'x6sfouap',
     dataset: 'production',
     useCdn: true, // set to `false` to bypass the edge cache
     apiVersion: '2023-10-14', // use current date (YYYY-MM-DD) to target the latest API version
     // token: process.env.SANITY_SECRET_TOKEN // Only if you want to update content with the client
-})
+});
